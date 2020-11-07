59ad9af9f6e4e2b59e38106cad64ef0dd7bf9227
omerates760 <33201732+omerates760@users.noreply.github.com>
2020-11-07 12:20:01 +0300

    bottom navigation in activity added.

--- app/src/main/java/com/monofire/appcentchallange/ui/MainActivity.kt
+++ app/src/main/java/com/monofire/appcentchallange/ui/MainActivity.kt
@@ -2,11 +2,34 @@ package com.monofire.appcentchallange.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.monofire.appcentchallange.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        // Finding the Navigation Controller
+        var navController = findNavController(R.id.nav_host)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavView.setupWithNavController(navController)
+
+        // Setting Up ActionBar with Navigation Controller
+        var appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf (
+                R.id.homeFragment,
+                R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
